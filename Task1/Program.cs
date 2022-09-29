a6f49c5900b2592671b6789cdbd7bc92e820c9f0
Dmitrii <razuhindm@gmail.com>
2022-09-29 20:54:44 +0300

    Вторая задача семинара 5

---
+++ Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using static System.Console;
+Clear();
+Write("Введите размер массива: ");
+int lenght = int.Parse(ReadLine()!);
+Write("Введите мин значение массива: ");
+int min = int.Parse(ReadLine()!);
+Write("Введите макс значение массива: ");
+int max = int.Parse(ReadLine()!);
+int[] array = GetArray(lenght, min, max);
+WriteLine($"[{String.Join(" | ", array)}]");
+
+ChangeToOpposite(array);
+
+WriteLine($"[{String.Join(" | ", array)}]");
+
+
+
+
+
+void ChangeToOpposite(int[] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i]=-array[i];
+    }
+}
+
+
+
+
+
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue+1);
+    }
+    return result;
+}
+
