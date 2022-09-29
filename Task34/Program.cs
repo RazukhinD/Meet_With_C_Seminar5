90a644df3ed50ffa68ee932736929059dadd5c52
Dmitrii <razuhindm@gmail.com>
2022-09-29 22:59:16 +0300

    Первая домашняя задача пятого семинара

---
+++ Task34/Program.cs
@@ -0,0 +1,45 @@
+﻿ //Задайте массив заполненный случайными положительными трёхзначными числами. 
+ //Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+
+using static System.Console;
+Clear();
+Write("Введите размер массива: ");
+int lenght = int.Parse(ReadLine()!);
+int[] array = GetArray(lenght);
+WriteLine($"[{String.Join(" | ", array)}]");
+HowManyEven(array);
+
+
+
+
+
+
+
+int [] GetArray (int size)
+{
+    Random rnd = new Random();
+    int [] result = new int [size];
+    for (int i=0;i<result.Length;i++)
+    {
+        result[i] = rnd.Next(100,1000);
+    }
+return result;
+}
+
+void HowManyEven (int [] array)
+{
+    int count = 0;
+    for (int i=0;i<array.Length;i++)
+    {
+        if (array[i]%2 == 0)
+        {
+            count+=1;
+        }
+    }
+    WriteLine($"Количество четных элементов массива: {count}");
+}
+
+
+
